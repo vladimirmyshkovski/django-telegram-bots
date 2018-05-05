03caf8e3df1a546d3bf7235d3cb74b1a69d9abd1
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2018-05-05 18:28:16 +0300

    added toggle_subscribe_button

--- telegram_bots/templatetags/telegram_bots_tags.py
+++ telegram_bots/templatetags/telegram_bots_tags.py
@@ -28,3 +28,24 @@ def unsubscribe_button(user, bot, button_class, button_text):
         'button_class': button_class,
         'button_text': button_text
     }
+
+
+@register.inclusion_tag('telegram_bots/button.html')
+def toggle_subscribe_button(user, bot, button_class):
+    authorization, created = Authorization.objects.get_or_create(
+        user=user.telegramuser,
+        bot=bot
+        )
+    if authorization.is_active:
+        data = {
+            'url': authorization.deactivation_url,
+            'button_class': button_class,
+            'button_text': 'Unsubscribe'
+        }
+    else:
+        data = {
+            'url': authorization.activation_url,
+            'button_class': button_class,
+            'button_text': 'Subscribe'
+        }
+    return data
