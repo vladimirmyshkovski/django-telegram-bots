07a8c0984ac7237a5ba8bb260420891cc81a3f44
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2018-05-30 22:34:33 +0300

    added user_is_subscribed template_tag

--- telegram_bots/templatetags/telegram_bots.py
+++ telegram_bots/templatetags/telegram_bots.py
@@ -1,15 +1,40 @@
 from django import template
-from ..models import Authorization
+from django.contrib.auth import get_user_model
+from ..models import Authorization, Bot
 
+User = get_user_model()
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def user_is_subscribed(context, user, bot):
+    assert isinstance(user, User), (
+        'user must be isinstance of User model'
+    )
+    assert isinstance(bot, Bot), (
+        'bot must be isinstance of Bot model'
+    )
+    authorization = Authorization.objects.filter(
+        user=user.telegramuser,
+        bot=bot
+    ).first()
+    if authorization:
+        return True
+    return False
 
 
 @register.inclusion_tag('telegram_bots/button.html')
 def subscribe_button(user, bot, button_class, button_text):
+    assert isinstance(user, User), (
+        'user must be isinstance of User model'
+    )
+    assert isinstance(bot, Bot), (
+        'bot must be isinstance of Bot model'
+    )
     authorization, created = Authorization.objects.get_or_create(
         user=user.telegramuser,
         bot=bot
-        )
+    )
     return {
         'url': authorization.activation_url,
         'button_class': button_class,
@@ -19,10 +44,16 @@ def subscribe_button(user, bot, button_class, button_text):
 
 @register.inclusion_tag('telegram_bots/button.html')
 def unsubscribe_button(user, bot, button_class, button_text):
+    assert isinstance(user, User), (
+        'user must be isinstance of User model'
+    )
+    assert isinstance(bot, Bot), (
+        'bot must be isinstance of Bot model'
+    )
     authorization, created = Authorization.objects.get_or_create(
         user=user.telegramuser,
         bot=bot
-        )
+    )
     return {
         'url': authorization.deactivation_url,
         'button_class': button_class,
@@ -32,6 +63,12 @@ def unsubscribe_button(user, bot, button_class, button_text):
 
 @register.inclusion_tag('telegram_bots/button.html')
 def toggle_subscribe_button(user, bot, button_class):
+    assert isinstance(user, User), (
+        'user must be isinstance of User model'
+    )
+    assert isinstance(bot, Bot), (
+        'bot must be isinstance of Bot model'
+    )
     authorization, created = Authorization.objects.get_or_create(
         user=user.telegramuser,
         bot=bot
